9fcf702f35fa5196faac825dc74c7001a665a474
jim29800 <jbouvier1173@gmail.com>
2018-02-02 00:56:37 +0100

    Recherche dans le titre (JS)

--- web/js/sort.js
+++ web/js/sort.js
@@ -7,3 +7,29 @@ table.addEventListener('click', function (e) {
     tableHeader.setAttribute('data-order', order);
     tinysort(table.querySelectorAll('tr'), { selector: 'td:nth-child(' + (Array.prototype.indexOf.call(table.querySelectorAll('th'), tableHeader) + 1) + ')', order: order });
 }); 
+
+
+
+// déterminer l'index des colonnes les colonnes
+var colonnes = {};
+$("#ListCoder thead th").each(function (index, th) {
+    colonnes[index] = $(th).text();
+}
+);
+// faire la recherche dans le tableau
+$("#search").keyup(function () {
+    var mots = $(this).val().toLowerCase().split(" ");
+    $("#ListCoder tbody tr").each(function (index, tr) {
+        if (mots[0].length > 0) $(tr).hide(); else $(tr).show();
+        $("td", tr).each(function (index, td) {
+            if (colonnes[index] in { ' Titre': true  }) {
+                for (mot in mots) {
+                    if (mots[mot].length > 0 && $(td).text().toLowerCase().indexOf(mots[mot]) >= 0) {
+                        $(tr).show();
+                        return false;
+                    }
+                }
+            }
+        });
+    });
+});
