725afb846df32519a0cd7145c3ab04be625a258d
jim29800 <jbouvier1173@gmail.com>
2018-02-01 11:49:26 +0100

    création des entités sans relation

---
+++ src/AppBundle/Entity/Annonce.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Annonce
+ *
+ * @ORM\Table(name="annonce")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\AnnonceRepository")
+ */
+class Annonce
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="photo", type="string", length=255)
+     */
+    private $photo;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="nombre_piece", type="integer")
+     */
+    private $nombrePiece;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Annonce
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Annonce
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set photo
+     *
+     * @param string $photo
+     *
+     * @return Annonce
+     */
+    public function setPhoto($photo)
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    /**
+     * Get photo
+     *
+     * @return string
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    /**
+     * Set nombrePiece
+     *
+     * @param integer $nombrePiece
+     *
+     * @return Annonce
+     */
+    public function setNombrePiece($nombrePiece)
+    {
+        $this->nombrePiece = $nombrePiece;
+
+        return $this;
+    }
+
+    /**
+     * Get nombrePiece
+     *
+     * @return int
+     */
+    public function getNombrePiece()
+    {
+        return $this->nombrePiece;
+    }
+}
+
